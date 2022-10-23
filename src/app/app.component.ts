71e55ab7458df5665422e21deaf67a776f9a5f3d
bergony.bandeira <bergony.bandeira@quark.tec.br>
2022-10-23 18:23:02 -0300

    [bergony] Angualar iniciado projeto e trocado o componete template pelo hello wor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //template: `<p>Hello world</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+  ],
+ // styles: ['']
 })
+
 export class AppComponent {
-  title = 'basics';
+
 }
